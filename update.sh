ca57bac3a5bfb9f4c7f10a626177b4991d85860c
Sulaiman Bello <sbello1112@gmail.com>
2022-05-19 17:42:50 +0100

    Elegant update

--- update.sh
+++ update.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+destination1="./android/app/src/main/AndroidManifest.xml"
+destination2="./android/app/build.gradle"
+destination3="./android/build.gradle"
+destination4="./android/gradle/wrapper/gradle-wrapper.properties"
+destination5="./pubspec.yaml"
+destination6="./lib/data/web_client.dart"
 
-mv -f ./update/AndroidManifest.xml ./android/app/src/main/AndroidManifest.xml
-mv -f ./update/app.build.gradle ./android/app/build.gradle
-mv -f ./update/project.build.gradle ./android/build.gradle
-mv -f ./update/gradle-wrapper.properties ./android/gradle/wrapper/gradle-wrapper.properties
-mv -f ./update/pubspec.yaml ./pubspec.yaml
-mv -f ./update/web_client.dart ./lib/data/web_client.dart
+mv -f ./update/AndroidManifest.xml ${destination1}
+echo "Updated ${destination1}"
+mv -f ./update/app.build.gradle ${destination2}
+echo "Updated ${destination2"
+mv -f ./update/project.build.gradle ${destination3}
+echo "Updated ${destination3}"
+mv -f ./update/gradle-wrapper.properties ${destination4}
+echo "Updated ${destination4}"
+mv -f ./update/pubspec.yaml ${destination5}
+echo "Updated ${destination5}"
+mv -f ./update/web_client.dart ${destination6}
+echo "Updated ${destination6}"
 echo "Successfully completed"
